821bf51c12ebd130b9d063dc6884ee8cb5f2b96c
Nathaniel <nathanielbn@gmail.com>
2025-02-18 11:37:09 +0100

    more-todo

--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -2,7 +2,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTodoDto } from './create-todo.dto';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {
-    description: string;
-    title: any;
-}
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
--- src/todo/dto/update.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { IsEnum } from "class-validator";
-import { todoRole } from "../enum/todo.role.enum";
-
-export class updateRoleDto{
-    @IsEnum(todoRole)
-    role: todoRole;
-}
