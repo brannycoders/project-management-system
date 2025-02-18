aa6c80f54251f4ced28f63f4232ed493d721b549
Nathaniel <nathanielbn@gmail.com>
2025-02-18 09:33:08 +0100

    todo-update

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,4 +1,45 @@
+/* eslint-disable prettier/prettier */
+import { IsNotEmpty, IsString } from "class-validator";
+
+/* eslint-disable prettier/prettier */
 export class CreateTodoDto {
+  @IsNotEmpty({message: 'Sorry this field cannot be empty, please fill it'})
+  @IsString()
   description: string;
+
+  @IsNotEmpty({message: 'Sorry this field cannot be empty, please fill it'})
+  @IsString()
   title: any;
 }
+
+
+
+
+/* eslint-disable prettier/prettier */
+
+// import { IsNotEmpty, IsEmail, MinLength, Matches, IsOptional, MaxLength, IsString } from "class-validator";
+// import { userRole } from "../enum/user.role.enum";
+
+
+
+// export class CreateUserDto {
+//     @IsNotEmpty({ message: 'sorry this field is not empty, kindly fill it'})
+//     @IsString()
+//     name: string;
+
+//     @IsNotEmpty()
+//     @IsEmail()
+//     email: string;
+
+
+//     @IsNotEmpty()
+//     @MinLength(8, {message: 'sorry you must put in 8 character'})
+//     @MaxLength(16, {message: 'password should not be more  than 16 characters'})
+//     @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^da-zA-Z]).{8,}$/, {message: 'password must contain at ;east one uppercase, one lowercase and special characters'})
+//     password: string;
+
+
+
+//     @IsOptional()
+//     role: userRole
+// }
--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -1,4 +1,8 @@
+/* eslint-disable prettier/prettier */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTodoDto } from './create-todo.dto';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {
+    description: string;
+    title: any;
+}
